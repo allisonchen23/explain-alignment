85e016ffe15155f15780fd0ab3d5b9a0e66c8792
Allison Chen <allisonchen@princeton.edu>
2023-05-15 15:37:00 -0400

    created explainer from CAVs; rerunning unscaled CAVs

--- src/utils/attribute_utils.py
+++ src/utils/attribute_utils.py
@@ -1,9 +1,10 @@
 import numpy as np
 import os, sys
 from tqdm import tqdm
+from sklearn.linear_model import LogisticRegression
 
 sys.path.insert(0, 'src')
-
+from utils.utils import informal_log
 
 def get_one_hot_attributes(data, paths, n_attr, splits=['train', 'val', 'test']):
     attributes = {}
@@ -77,3 +78,88 @@ def get_frequent_attributes(attributes,
         freq_attributes_dict[split] = freq_attributes
 
     return freq_attributes_dict, freq_attributes_one_hot
+
+def hyperparam_search_l1(train_features, train_labels, val_features, val_labels,
+                      Cs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 3, 5]):
+    best_clf = None
+    best_acc = 0
+
+    for c in Cs:
+        clf = LogisticRegression(solver='liblinear', C=c, penalty='l1')
+        clf.fit(train_features, train_labels)
+        score = clf.score(val_features, val_labels)
+        if score>best_acc:
+            best_acc = score
+            best_clf = clf
+            print("Best accuracy: {} Regularization: {}".format(score, c))
+
+    return best_clf
+
+def hyperparam_search(train_features,
+                                  train_labels,
+                                  val_features,
+                                  val_labels,
+                                  regularization,
+                                  solver,
+                                  scaler=None,
+                                  Cs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 3, 5],
+                                  log_path=None):
+    best_clf = None
+    best_acc = 0
+
+    if scaler is not None:
+        scaler.fit(train_features)
+        print("Scaler parameters: {}".format(scaler.get_params()))
+        train_features = scaler.transform(train_features)
+        val_features = scaler.transform(val_features)
+    for c in Cs:
+        clf = LogisticRegression(solver=solver, C=c, penalty=regularization)
+        clf.fit(train_features, train_labels)
+        score = clf.score(val_features, val_labels)
+        if score>best_acc:
+            best_acc = score
+            best_clf = clf
+            informal_log("Best accuracy: {} Regularization: {}".format(score, c), log_path)
+
+    return best_clf
+
+def partition_paths_by_congruency(explainer_predictions,
+                                  model_predictions,
+                                  paths):
+    '''
+    Given list or arrays of explainer and model predictions, partition paths based on if predictions align
+
+    Arg(s):
+        explainer_predictions : N-length np.array
+            predictions output by the explainer model
+        model_predictions : N-length np.array
+            predictions output by the model
+        paths : N-length list
+            paths of images corresponding to each data point
+
+    Returns:
+        dictionary : dict[str] : list
+            key: 'congruent' or 'incongruent'
+            value: list of paths
+    '''
+    n_samples = len(paths)
+    assert len(explainer_predictions) == n_samples
+    assert len(model_predictions) == n_samples, "Length of model predictions {} doesn't match n_samples {}".format(
+        len(model_predictions), n_samples
+    )
+
+    incongruent_paths = []
+    congruent_paths = []
+
+    for explainer_prediction, model_prediction, path in tqdm(zip(
+        explainer_predictions, model_predictions, paths
+    )):
+        if explainer_prediction == model_prediction:
+            congruent_paths.append(path)
+        else:
+            incongruent_paths.append(path)
+
+    return {
+        'congruent': congruent_paths,
+        'incongruent': incongruent_paths
+    }
