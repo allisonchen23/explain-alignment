538e9dc3fcb888c646f1234f210114bb58401977
Allison Chen <allisonchen@princeton.edu>
2023-06-05 15:57:09 -0400

    ran linear classifier on kmeans with dense features

--- experimental_scripts/dense_sift_kmeans_search.py
+++ experimental_scripts/dense_sift_kmeans_search.py
@@ -1,5 +1,5 @@
 import os, sys
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, MiniBatchKMeans
 import argparse
 from datetime import datetime
 import torch
@@ -16,27 +16,28 @@ np.random.seed(seed)
 
 def dense_sift_kmeans_search(debug, step_size, sigma, 
                              n_samples=None,
-                             image_size=(32, 32)):
+                             image_size=(32, 32),
+                             ks=[500, 1000, 1500, 2000, 2500],
+                             mini_batch_size=1024):
     sift_data_path = os.path.join("saved", "cifar10", 
                               'sift_{}_{}_sigma{}'.format(image_size[0], image_size[1], sigma), 
                               'dense_stride_{}'.format(step_size),
                               'sift_keypoints_descriptors.pth')
     timestamp = datetime.now().strftime(r"%m%d_%H%M%S")
-    save_dir = os.path.dirname(sift_data_path, timestamp)
+    save_dir = os.path.join(os.path.dirname(sift_data_path), timestamp)
     
     log_path = os.path.join(save_dir, 'sift_kmeans_log.txt')
     informal_log("KMeans hyperparameter search", log_path)
 
     # Load descriptors
-    informal_log("[{}] Loading features from {}".format(
-        datetime.now().strftime(r'%m%d_%H%M%S'), sift_data_path), log_path)
+    # informal_log("[{}] Loading features from {}".format(
+    #     datetime.now().strftime(r'%m%d_%H%M%S'), sift_data_path), log_path)
 #     sift_data = torch.load(sift_data_path)
 
 #     train_descriptors = sift_data['train']['descriptors']
 #     n_data = len(train_descriptors)
 
     # Randomly sample if needed to reduce number of examples
-    
     if n_samples is not None:
         sampled_descriptor_save_path = os.path.join(save_dir, '{}_train_sift_keypoint_descriptors.pth'.format(n_samples))
         if os.path.exists(sampled_descriptor_save_path):
@@ -81,15 +82,25 @@ def dense_sift_kmeans_search(debug, step_size, sigma,
 
     if debug:
         ks = [5]
-    else:
-        ks = [500, 1000, 1500, 2000, 2500]
+        
     n_init = 10
     inertias = []
-
-
+    # Run KMeans for each
     for k in ks:
         informal_log("[{}] Calculating k-means for k={}".format(
             datetime.now().strftime(r'%m%d_%H%M%S'), k), log_path)
+        if mini_batch_size > 0:
+            if debug:
+                kmeans = MiniBatchKMeans(
+                    n_clusters=k,
+                    n_init=n_init,
+                    batch_size=mini_batch_size,
+                    max_iter=5)
+            else:
+                kmeans = MiniBatchKMeans(
+                    n_clusters=k,
+                    n_init=n_init,
+                    batch_size=mini_batch_size)
         if debug:
             kmeans = KMeans(n_clusters=k, n_init=n_init, max_iter=5)
         else:
@@ -98,7 +109,7 @@ def dense_sift_kmeans_search(debug, step_size, sigma,
         informal_log("[{}] Inertia: {}".format(
             datetime.now().strftime(r'%m%d_%H%M%S'), kmeans.inertia_), log_path)
         inertias.append(kmeans.inertia_)
-        kmeans_save_path = os.path.join(os.path.dirname(sift_data_path),
+        kmeans_save_path = os.path.join(save_dir,
                                     '{}means'.format(k), 
                                     'descriptor_kmeans.pth')
         ensure_dir(os.path.dirname(kmeans_save_path))
@@ -123,14 +134,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true')
     parser.add_argument('--step_size', type=int, required=True)
-    parser.add_argument('--n_samples', type=int, default=50000)
+    parser.add_argument('--n_samples', type=int, default=None)
     parser.add_argument('--sigma', type=float, required=True)
+    parser.add_argument('--mini_batch_size', type=int, default=-1)
+    parser.add_argument('--ks', nargs='+', type=int, default=[500, 1000, 1500, 2000, 2500])
     
     args = parser.parse_args()
     dense_sift_kmeans_search(
         debug=args.debug,
         step_size=args.step_size,
         n_samples=args.n_samples,
-        sigma=args.sigma)
+        sigma=args.sigma,
+        ks=args.ks,
+        mini_batch_size=args.mini_batch_size)
     
     
