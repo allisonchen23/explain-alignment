9a92f6e5ed58d465d43275ab2a3936708f714d6e
Allison Chen <allisonchen@princeton.edu>
2023-07-10 14:49:30 -0400

    made separate train files for pixel and SIFT explainers

--- bash/train_cifar_SIFT_explainer_hparam.sh
+++ bash/train_cifar_SIFT_explainer_hparam.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 python explainer_hparam_search.py \
---config configs/train_cifar_explainer_hparam.json \
+--config configs/train_cifar_SIFT_explainer_hparam.json \
 --learning_rates 1e-4 1e-3 5e-2 1e-2 5e-1 1e-1 \
 --weight_decays 0 1e-1 1e-2 1e-3 \
---build_save_dir
+--build_save_dir \
+--debug
