74cab7cf72e39c20be05a49c98562fe4e0db2852
Allison Chen <allisonchen@princeton.edu>
2023-08-04 15:57:11 -0400

    running repeated trials for many ACE, CAV, labeled concepts

--- bash/explainer_hparam_search/wandb_hparam_search.sh
+++ bash/explainer_hparam_search/wandb_hparam_search.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=8    # Specify the number of CPUs your task will need.
 #SBATCH --gres=gpu:rtx_3090:1          # the number of GPUs requested
 #SBATCH --mem=50G             # memory
-#SBATCH -o slurm_logs/%x_%i_out.txt         # send stdout to outfile
-#SBATCH -e slurm_logs/%x_%i_err.txt         # send stderr to errfile
+#SBATCH -o slurm_logs/%j_%x_out.txt         # send stdout to outfile
+#SBATCH -e slurm_logs/%j_%x_err.txt         # send stderr to errfile
 #SBATCH -t 72:00:00           # time requested in hour:minute:second
 
 CONFIG_PATH="configs/explainer_hparam_search/$1.json"
