d50b89c225c529166e8efa211e3580cdfd5aeadd
Allison Chen <allisonchen@princeton.edu>
2023-08-15 15:55:51 -0400

    added/moved config and bash files to clean up

--- bash/explainer_hparam_search/wandb_train_cifar_SIFT_explainer_hparam.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python explainer_hparam_search_wandb.py \
---config configs/wandb_train_cifar_SIFT_explainer_hparam.json \
---train_script_path src/train_hparam_search.py \
---learning_rates 1e-4 1e-3 5e-2 1e-2 5e-1 1e-1 \
---weight_decays 0 1e-1 1e-2 1e-3 \
